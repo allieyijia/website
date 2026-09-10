cc883bbb62adb93d8dbb6e211da18a4cf15d4385
allieyijia <alliehe2015@gmail.com>
2026-09-09 23:28:44 -0400

    added synesthesia

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 (function () {
   var page = location.pathname.split('/').pop() || 'index.html';
-  var workPages = ['ux.html', 'ups.html', 'beacon.html', 'pawfect.html', 'savor.html', 'abg.html', 'abg-research.html', 'abg-artet.html'];
+  var workPages = ['ux.html', 'ups.html', 'beacon.html', 'pawfect.html', 'savor.html', 'synesthesia.html', 'abg.html', 'abg-research.html', 'abg-artet.html'];
 
   function link(href, label, active) {
     return '<a href="' + href + '"' + (active ? ' class="active"' : '') + '>' + label + '</a>';
